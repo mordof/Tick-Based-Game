02c853ccd71f7def890a7c2206a0235936ae01af
Mike Winger <mike.winger87@gmail.com>
2015-01-01 09:22:20 -0500

    Updated Stars model with 2d index

--- server/backend/models/star.rb
+++ server/backend/models/star.rb
@@ -3,4 +3,7 @@ class Star
   field :color, type: String
   field :count, type: Integer
   field :offset, type: Hash
+  field :location, type: Array
+
+  index({ location: '2d' }, { min: -400, max: 400 })
 end
--- server/frontend/lib/models.js
+++ server/frontend/lib/models.js
@@ -8,8 +8,11 @@ var starSchema = new mongoose.Schema({
   offset: { 
     x: Number, 
     y: Number 
-  }
+  },
+  location: [Number]
 })
+
+starSchema.index({ location: '2d' }, { min: -400, max: 400 })
 
 module.exports  = {
   Star: mongoose.model('Star', starSchema)
