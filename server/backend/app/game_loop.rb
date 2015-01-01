63ec18bd6437ef49705bec39dae7f435b7ab0bea
Mike Winger <mike.winger87@gmail.com>
2015-01-01 09:58:17 -0500

    Modified server loop to include all stars

--- server/backend/app/game_loop.rb
+++ server/backend/app/game_loop.rb
@@ -1,11 +1,8 @@
 module GameLoop
   def self.run
-    red_star = Star.where( color: 'red' ).first
-    red_star.count += (red_star.count / 4).to_i
-    red_star.save!
-
-    blue_star = Star.where( color: 'blue' ).first
-    blue_star.count += (blue_star.count / 4).to_i
-    blue_star.save!
+    Star.each do |star|
+      star.count += (star.count / 4).to_i
+      star.save!
+    end
   end
 end
