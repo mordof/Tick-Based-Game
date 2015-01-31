bbd0803e33ad15223c8e77212da804c49cf4e0d9
Micheal Winger <mike.winger87@gmail.com>
2015-01-31 10:10:48 -0500

    Added Unity client to repo

---
+++ client/unity/Assets/MakePlanets.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MakePlanets : MonoBehaviour {
+
+	public GameObject planet;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 5; i++) {
+			Instantiate(planet, new Vector3(Random.Range (-10, 10), 0, 0), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ client/unity/Assets/Networking.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Threading;
+using System.Collections;
+using System.Net.Sockets;
+using System.IO;
+
+public class Networking : MonoBehaviour {
+
+	public TcpClient client;
+	private bool clientClosed;
+
+	// Use this for initialization
+	void Start () {
+		clientClosed = false;
+		Thread t = new Thread (new ThreadStart (Comm));
+		t.Start ();
+	}
+
+	void OnApplicationQuit () {
+		if (clientClosed == false) {
+			CloseClient ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Comm(){
+		client = new TcpClient ("192.168.234.130", 3060);
+		
+		try {
+			Stream s = client.GetStream();
+			StreamReader sr = new StreamReader(s);
+			StreamWriter sw = new StreamWriter(s);
+			sw.AutoFlush = true;
+
+			sw.Write ("login|:{\"username\": \"mordof\"}:|");
+			sw.Write ("chat.public_message|:\"Test Chat Message\":|");
+			sw.Write ("viewport.get_grid:|");
+
+			var msg = sr.ReadLine ();
+
+			while(msg != null){
+				Debug.Log ("Message: " + msg);
+				msg = sr.ReadLine ();
+			}
+
+
+		} finally {
+			CloseClient ();
+			clientClosed = true;
+		}
+	}
+
+	void CloseClient() {
+		client.Client.Disconnect (false);
+		client.GetStream ().Close ();
+		client.Close ();
+		Debug.Log ("Client Connection Closed");
+	}
+}
